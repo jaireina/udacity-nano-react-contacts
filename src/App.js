b6e0fab11eb685a7aec82bb26641066ad3a316bf
Jair Reina <Jair.Reina@digitaslbi.com>
2017-11-07 22:57:07 -0600

    adding Route to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Route} from 'react-router-dom';
 import ListContacts from './ListContacts.jsx';
 import * as ContactsAPI from './utils/ContactsAPI';
 import CreateContact from './CreateContact';
@@ -35,19 +36,15 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        
-        {this.state.screen === 'list' && (
+        <Route exact path="/" render={()=>(
           <ListContacts 
             contacts={this.state.contacts} 
             onDeleteContact={this.deleteContact}
             onCreateContact={this.showCreateContact}
           />
-        )}
+        )} />
         
-        {this.state.screen === 'create' && (
-          <CreateContact />
-        )}
-
+        <Route path="/create" component={CreateContact} />
       </div>
     );
   }
